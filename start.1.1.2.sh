ffdc785df60167c10c69b638a09cb917c495ea4b
arrowdannad <13862931614@163.com>
2022-09-04 01:13:02 +0800

    1.1.2
    紧急更新：
    修复：
    自动重启时会接收莫名输入导致的自动退出循环；
    增肌：
    增强防误触导致的自动重启退出；

--- start.1.1.2.sh
+++ start.1.1.2.sh
@@ -14,13 +14,24 @@ if [ -f qwq ];then
 java路径/java -Xms1024M -Xmx3000M -jar 核心名称.jar -nogui
     sleep 1s && clear
     echo "20秒后重启服务器"
-    read -N 1 -t 19 -p"等待时间内可以按2来关闭自动重启并退出  " a
-    [ "$a" != "" ] && echo "  检测到输入，退出自动重启" && sed -i 's/1/0/g' qwq || echo "  未检测到输入，执行自动重启"
+    read -N 3 -t 10 -p"等待时间内可以输入'yes'来关闭自动重启并退出  " a
+    acse $a in
+    [yY][eE][sS]|[yY] ) {
+    echo "  检测到输入，退出自动重启"
+    sed -i 's/1/0/g' qwq
+    break
+    };;
+    * ) {
+        sleep 10s
+        echo "  未检测到输入，执行自动重启"
+    };;
+    esac
+    echo "  未检测到输入，执行自动重启"
     else 
     sleep 1s && clear
     echo "请输入数字1~3选择模式"
     echo "1:自动重启模式" && echo "2:单次启动模式" && echo "3:退出脚本"
-read -N 1 shuru
+read shuru
 case "$shuru" in
     1 ) {
         echo "已选择（自动重启模式）" && echo "正在配置文件"
@@ -36,19 +47,18 @@ java路径/java -Xms1024M -Xmx3000M -jar 核心名称.jar -nogui
 };;
     3 ) {
         clear && sed -i 's/1/0/g' qwq
-        echo "退出脚本"
-        exit
+        echo "退出脚本" && exit
 };;
-* ) echo "错误的参数，请重新输入";;
+* ) echo "异常参数，请重新输入";;
 esac
     fi
     done
 else
-    echo "未检测到文件，已自动生成"
-    echo "正在重启脚本"
+    echo "未检测到文件，已自动生成" && sleep 2s
 awd_mx_wenjian=0
 cat <<awd_mx_chuanru>qwq
 $awd_mx_wenjian
 awd_mx_chuanru
+    echo "正在重启脚本"
 fi
 done
